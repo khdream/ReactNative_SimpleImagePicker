59376027113dc80a25b04804e0d2d4dfe27b714f
Team work <>
2021-01-22 21:49:21 -0700

    1.22-1
    Co-Authored-By: khdream <khdream94@yandex.com>

--- App.js
+++ App.js
@@ -29,30 +29,10 @@ export default class App extends React.Component {
     });
   }
 
-  handleChoosePhoto_2 = () => {
-    let options = {};
-    launchCamera(options, response => {
-      console.log({ response });
-      if (response.didCancel) {
-        console.log('User cancelled photo picker');
-        Alert.alert('You did not select any image');
-      } else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
-      } else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton);
-      } else {
-        let source = { uri: response.uri };
-        console.log({ source });
-      }
-    });
-  }
-
   render() {
     return (
       <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
           <Button title="launch Image Library" onPress={this.handleChoosePhoto_1}/>
-          <Text></Text>
-          <Button title="launch Camera" onPress={this.handleChoosePhoto_2}/>
       </View>
     );
   }
